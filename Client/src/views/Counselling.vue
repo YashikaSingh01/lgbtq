e77ac05732939533ed43512d360d95c2a75067cd
zeeshaantandel <zeeshaan143tandel@gmail.com>
2023-04-26 11:54:26 +0530

    changes

--- Client/src/views/Counselling.vue
+++ Client/src/views/Counselling.vue
@@ -1,23 +1,25 @@
 <template>
     <div>
-        <v-carousel v-model="cycle" :show-arrows="false" :continuous="false" :cycle="cycle" hide-delimiter-background hide-delimiters height="90vh">
+        <v-carousel v-model="cycle" :show-arrows="false" :continuous="false" :cycle="cycle" hide-delimiter-background
+            hide-delimiters height="90vh">
             <v-carousel-item>
-            <v-sheet class="" color="rgba(248, 237, 237, 1)" height="" tile>
-                <v-row class="fill-height" align="center" justify="center">
-                    <v-col cols="6">
-                        <!-- <h3>--insert image--</h3> -->
-                        <v-img src="@/assets/images/pexels-cottonbro-studio-3693901.jpg" height="90vh"></v-img>
-                    </v-col>
-                    <v-col cols="6">
-                        <p class="carousel-text">Talk it Out!</p>
-                        <p class="carousel-text-small">
-                            Discover yourselves, discover love by joining a community of acceptance and support, where love
-                            is always in the air.<br>
-                            Talk it out loud with pride!
-                        </p>
-                    </v-col>
-                </v-row>
-            </v-sheet>
+                <v-sheet class="" color="rgba(248, 237, 237, 1)" height="" tile>
+                    <v-row class="fill-height" align="center" justify="center">
+                        <v-col cols="6">
+                            <!-- <h3>--insert image--</h3> -->
+                            <v-img src="@/assets/images/pexels-cottonbro-studio-3693901.jpg" height="90vh"></v-img>
+                        </v-col>
+                        <v-col cols="6">
+                            <p class="carousel-text">Talk it Out!</p>
+                            <p class="carousel-text-small">
+                                Discover yourselves, discover love by joining a community of acceptance and support, where
+                                love
+                                is always in the air.<br>
+                                Talk it out loud with pride!
+                            </p>
+                        </v-col>
+                    </v-row>
+                </v-sheet>
             </v-carousel-item>
         </v-carousel>
 
@@ -65,6 +67,47 @@
             </v-col>
         </v-row>
 
+        <v-row style="margin-top: 10vh;">
+            <v-col cols="10" offset="1" align-self="center">
+                <v-carousel v-model="cycle" :continuous="true" :cycle="cycle" delimiter-icon="mdi-minus"  hide-delimiter-background hide-delimeters 
+                delimiter-icon-cycle height="70vh">
+                    <v-carousel-item>
+                        <v-sheet class="" color="rgba(248, 237, 237, 0.7)" height="100%" tile>
+                            <v-row style="height: 100%;">
+                                <v-col cols="3" offset="1" align-self="center">
+                                    <v-card class="blog-card" elevation="10" height="65vh">
+                                        <v-img class="white--text align-end" height="60%" alt=""
+                                            src="@/assets/images/pexels-marta-branco-1173576.jpg">
+
+                                        </v-img>
+
+                                        <v-card-title style="word-break: keep-all">Don't know how to tell to your
+                                            parents?</v-card-title>
+
+                                        <v-card-subtitle class="pb-0">
+                                            3 minute read.
+                                        </v-card-subtitle>
+
+                                        <v-card-text class="text--primary">
+                                            Robyn Exton
+                                            <v-icon>mdi-circle-small</v-icon>
+                                            19th February 2018
+                                        </v-card-text>
+
+                                        <v-card-actions>
+                                            <v-btn color="rgba(109, 58, 69, 1)" text @click="openBlog1">
+                                                Read Now
+                                            </v-btn>
+                                        </v-card-actions>
+                                    </v-card>
+                                </v-col>
+                            </v-row>
+                        </v-sheet>
+                    </v-carousel-item>
+
+                </v-carousel>
+            </v-col>
+        </v-row>
 
 
     </div>
