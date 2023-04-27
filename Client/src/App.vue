318e35dd5db7163132fc07efc65c9047d4ac0139
YashikaSingh01 <86999014+YashikaSingh01@users.noreply.github.com>
2023-04-28 00:36:24 +0530

    Changes AppBar

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="rgba(199, 199, 199, 0.5)"
-      dark
-    >
+    <v-app-bar app color="rgba(199, 199, 199, 0.5)" dark>
       <div class="d-flex align-center main-icon">
-        <v-img
-          alt="Logo"
-          class="shrink mr-2"
-          contain
-          src="@/assets/logo.png"
-          transition="scale-transition"
-          width="95"
-        />
+        <v-btn icon x-large v-on="on" @click="goToLandingPage">
+          <v-avatar tile size="90" style="margin-left: 5vh;">
+            <v-img alt="Logo" class="shrink mr-2" contain src="@/assets/logo.png" transition="scale-transition"
+              width="95" />
+          </v-avatar>
+        </v-btn>
       </div>
 
       <!-- <v-spacer></v-spacer>
@@ -27,11 +21,11 @@
       <p class="appbar-text">Counselling</p> -->
 
       <v-spacer></v-spacer>
-    <div class="d-flex align-center appbar-text">
-      <div class="mr-2" >
-        <!-- <span @click="goToBlogs">Blogs</span> -->
-        <v-btn text color="rgba(109, 58, 69, 1)" style="font-size: 100%;" @click="goToBlogs">Blogs</v-btn>
-        <!-- <v-btn
+      <div class="d-flex align-center appbar-text">
+        <div class="mr-2">
+          <!-- <span @click="goToBlogs">Blogs</span> -->
+          <v-btn text color="rgba(109, 58, 69, 1)" style="font-size: 100%;" @click="goToBlogs">Blogs</v-btn>
+          <!-- <v-btn
         class="ma-1"
         color="rgba(109, 58, 69, 1)"
         plain
@@ -39,19 +33,64 @@
       >
         Blogs
       </v-btn> -->
-      </div>
-      
-      <div class="mr-2" style="margin-left: 3vh;">
-        <!-- <span @click="goToCounselling">Counselling</span> -->
-        <v-btn text color="rgba(109, 58, 69, 1)" style="font-size: 100%;" @click="goToCounselling">COUNSELLING</v-btn>
+        </div>
+
+        <div class="mr-2" style="margin-left: 3vh;">
+          <!-- <span @click="goToCounselling">Counselling</span> -->
+          <v-btn text color="rgba(109, 58, 69, 1)" style="font-size: 100%;" @click="goToCounselling">COUNSELLING</v-btn>
+        </div>
+
+
+        <div class="mr-2" style="margin-left: 3vh;">
+          <!-- <v-container fluid style="height: 300px"> -->
+          <v-row justify="center">
+            <v-menu bottom min-width="200px" rounded offset-y>
+              <template v-slot:activator="{ on }">
+                <v-btn icon x-large v-on="on">
+                  <v-avatar color="brown" size="40">
+                    <v-icon dark>
+                      mdi-account-circle
+                    </v-icon>
+                    <span class="white--text text-h5"></span>
+                  </v-avatar>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-list-item-content class="justify-center">
+                  <div class="mx-auto text-center">
+                    <v-avatar color="brown">
+                      <span class="white--text text-h5">{{ user.initials }}</span>
+                    </v-avatar>
+                    <h3>{{ user.fullName }}</h3>
+                    <p class="text-caption mt-1">
+                      {{ user.email }}
+                    </p>
+                    <v-divider class="my-3"></v-divider>
+                    <v-btn depressed rounded text>
+                      Edit Account
+                    </v-btn>
+                    <v-divider class="my-3"></v-divider>
+                    <v-btn depressed rounded text>
+                      Logout
+                    </v-btn>
+                  </div>
+                </v-list-item-content>
+              </v-card>
+            </v-menu>
+          </v-row>
+          <!-- </v-container> -->
+        </div>
+
+
+
+
+
+        <div class="mr-2" style="margin-left: 3vh;">
+          <v-btn text color="rgba(109, 58, 69, 1)" style="font-size: 100%;" @click="goToLogin">LOGIN</v-btn>
+        </div>
       </div>
 
-    <div class="mr-2" style="margin-left: 3vh;">
-        <v-btn text color="rgba(109, 58, 69, 1)" style="font-size: 100%;" @click="goToLogin">LOGIN</v-btn>
-      </div>
-    </div>
 
-      
 
       <!-- <v-btn color="rgba(109, 58, 69, 1)" elevation="2" rounded>
         <span class="mr-2">Download here</span>
@@ -62,21 +101,20 @@
         target="_blank"
         text
       > -->
-        <!-- <v-icon>mdi-open-in-new</v-icon> </v-btn>-->
+      <!-- <v-icon>mdi-open-in-new</v-icon> </v-btn>-->
 
     </v-app-bar>
 
-  
+
 
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
 
     <Footer style="margin-top: 5%;"></Footer>
 
   </v-app>
-  
 </template>
 
 <script>
@@ -85,8 +123,11 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
-    
+    user: {
+      initials: 'SG',
+      fullName: 'Shawn Geller',
+      email: 'shawngeller@gmail.com',
+    },
   }),
 
   components: {
@@ -103,6 +144,9 @@ export default {
     goToLogin() {
       this.$router.push('/login')
     },
+    goToLandingPage() {
+      this.$router.push('/')
+    },
 
   },
 };
@@ -117,7 +161,7 @@ export default {
   left: 1%;
 }
 
-.appbar-text{
+.appbar-text {
   color: rgba(109, 58, 69, 1);
 
 }
